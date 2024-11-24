9ad98e39970f46d541200e350532298edf8cab94
Gustavo Cazzine <cazzinengustavo@gmail.com>
2024-11-24 12:13:32 -0300

    att aula js

---
+++ Javascript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Boas Vindas ao jogo do número secreto');
+
+let numeroSecreto = 29;
+let chuteNumeroSecreto = prompt('Chute um número de 0 a 100: ');
+
+if (numeroSecreto == chuteNumeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto (29)');
+}
