636b061342322aa60498f4da8106e57d686ef990
Gustavo Cazzine <cazzinengustavo@gmail.com>
2024-12-11 19:24:44 -0300

    html att

--- Projetos_Treinos/Python/Registro_De_Vendas/__init__.py
+++
@@ -1,9 +0,0 @@
-class Cadastrar_Venda:
-    def __init__(self, Vendedor, Produto, Preço , Quantidade):
-        self.Vendedor = Vendedor
-        self.Produto = Produto
-        self.Preço = Preço
-        self. Quantidade = Quantidade
-
-    def __str__(self):
-         
