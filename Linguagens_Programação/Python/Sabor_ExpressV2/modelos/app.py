d6f3e96cd79f0b34eee6f71194c5a00819eeefa5
Gustavo Cazzine <cazzinengustavo@gmail.com>
2025-01-03 16:13:16 -0300

    atualizaçoes cursos alura

--- Linguagens_Programação/Python/Sabor_ExpressV2/modelos/app.py
+++ Linguagens_Programação/Python/Sabor_ExpressV2/modelos/app.py
@@ -1,9 +1,9 @@
 from modelos.restaurante import Restaurante
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Gu', 1)
+restaurante_praca.receber_avaliacao('Emy', 2)
 
 def main():
     Restaurante.listar_restaurantes()
